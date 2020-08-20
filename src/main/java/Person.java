0ca6adfb6518fe2288ec3208d3f8a6f3dae047c0
hcf <1941710278@qq.com>
2020-08-20 22:40:57 +0800

    up...

--- src/main/java/Person.java
+++ src/main/java/Person.java
@@ -5,12 +5,17 @@ public class Person {
 	Integer age; //用户1写的
 	
 	String hobby; //用户1编写了hobby
-	
+
+	/***
+	 * 窗口1在22:39做了修改
+	 */
+
+
+
 	public void print(){
 		System.out.println("用户1");
 	}
-	
-	
+
 	private void add(){
 
 	}
@@ -20,5 +25,9 @@ public class Person {
 	}
 	
 	private BigDecimal money;
+
+	/***
+	 * 窗口1在22:40做了修改
+	 */
 	
 }
