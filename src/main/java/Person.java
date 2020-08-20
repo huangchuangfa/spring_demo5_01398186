ec69a6f192a89861abdb7394d7db3a6b3bd88750
hcf <1941710278@qq.com>
2020-08-20 23:54:31 +0800

    up...

--- src/main/java/Person.java
+++ src/main/java/Person.java
@@ -1,11 +1,11 @@
 import java.math.BigDecimal;
 
 public class Person {
-	
-	String personName;
-	
+
+/*	String personName;
+ */
 	Integer age; //用户1写的
-	
+
 	String hobby; //用户1编写了hobby
 
 
@@ -17,23 +17,7 @@ public class Person {
 	 * subarray, and the {@code count} argument specifies the length of the
 	 * subarray.
 	 *
-	 * <p> Each {@code byte} in the subarray is converted to a {@code char} as
-	 * specified in the method above.
-	 *
-	 * @see  #String(byte[], int)
-	 * @see  #String(byte[], int, int, java.lang.String)
-	 * @see  #String(byte[], int, int, java.nio.charset.Charset)
-	 * @see  #String(byte[], int, int)
-	 * @see  #String(byte[], java.lang.String)
-	 * @see  #String(byte[], java.nio.charset.Charset)
-	 * @see  #String(byte[])
 	 */
-
-
-
-
-
-
 
 	public void print(){
 		System.out.println("用户1");
