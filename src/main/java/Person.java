6c214a116c7606d9161d628861b16e946b15633c
hcf <1941710278@qq.com>
2020-08-20 22:07:51 +0800

    用户1修改了Person.java

--- src/main/java/Person.java
+++ src/main/java/Person.java
@@ -1,7 +1,11 @@
 public class Person {
 	
 	String personName;
-	
+
+	String address;
+
+	String eMail;
+
 	Integer age; //用户1写的
 	
 	String hobby; //用户1编写了hobby
@@ -9,10 +13,9 @@ public class Person {
 	public void print(){
 		System.out.println("用户1");
 	}
-	
-	
-	
-	
+
 	private BigDecimal money;
+
+	private String sex;
 	
 }
