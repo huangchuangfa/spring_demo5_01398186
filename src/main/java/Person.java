a6a5d18ae57dda94bc5db884228338566c61277a
hcf <1941710278@qq.com>
2020-08-20 22:45:55 +0800

    用户1修改了Person.java

--- src/main/java/Person.java
+++ src/main/java/Person.java
@@ -5,7 +5,10 @@ public class Person {
 	Integer age; //用户1写的
 	
 	String hobby; //用户1编写了hobby
-	
+
+	/**
+	 * 窗口2在22:45做了修改
+	 */
 	public void print(){
 		System.out.println("用户1");
 	}
@@ -14,6 +17,12 @@ public class Person {
 	private void add(){
 
 	}
+
+	/**
+	 * 窗口2在22:46做了修改
+	 * @param age
+	 */
+
 
 	private void setAge(Integer age) {
 		this.age = age;
