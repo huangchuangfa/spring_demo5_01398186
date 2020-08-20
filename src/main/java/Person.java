2544be004751b82e22578310cf73a8a4b1055c9e
hcf <1941710278@qq.com>
2020-08-20 23:26:35 +0800

    用户1修改了Person.java

--- src/main/java/Person.java
+++ src/main/java/Person.java
@@ -15,10 +15,6 @@ public class Person {
 	 * 窗口1在23:07做了修改
 	 */
 
-
-
-
-
 	public void print(){
 		System.out.println("用户1");
 	}
@@ -27,32 +23,14 @@ public class Person {
 
 	}
 
-	//******************
-	/***
-	 * 窗口2在23:09做了修改
-	 */
-	//*********************
-
-
 	private void setAge(Integer age) {
 		this.age = age;
 	}
 	
 	private BigDecimal money;
 
-	/***
-	 * 窗口1在22:40做了修改
-	 */
-
-
 	/**
-	 * 窗口1在22：41做了修改
-	 */
-
-
-   //************************
-	/***
-	 * 窗口1在23:08做了修改
+	 * 123sdfsdafsda
 	 */
 
 
